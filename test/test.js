bc7a28f2c2a7033f6fe452d7ceb6e0f4ab9b7b39
joss <josselin54.chevalay@gmail.com>
2014-08-05 13:53:18 +0200

    i create unit test for attribute, class and character

--- test/test.js
+++ test/test.js
@@ -3,12 +3,38 @@
 */
 var Character = require("../lib/Character/Character.js");
 
+var Attribute = require("../lib/Attribute/Attribute.js");
+
+var Class = require("../lib/Attribute/Attribute.js");
+
 exports.testCharacter = function(test){
     var c = new Character("test");
     if (c.getName()=== "test") {
         test.ok(true, "yes");
-    } else{
+    } else {
         test.ok(false, "no");
+    }
+
+    test.done();
+};
+
+exports.testAttribute = function(test){
+    var a = new Attribute("life", "no die :p ");
+    if (a.getName() === "life") {
+        test.ok(true, "Attribute can instantiate and getName");
+    } else {
+        test.ok(false, "Attribute can't instantiate and getName");
+    }
+
+    test.done();
+};
+
+exports.testClass = function(test){
+    var c = new Class("life");
+    if (c.getName() === "life") {
+        test.ok(true, "Attribute can instantiate and getName");
+    } else {
+        test.ok(false, "Attribute can't instantiate and getName");
     }
 
     test.done();
