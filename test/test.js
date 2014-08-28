c5776f338249c1757baa00b897093121be507495
joss <josselin54.chevalay@gmail.com>
2014-08-28 13:59:28 +0200

    implement defineproperty

--- test/test.js
+++ test/test.js
@@ -9,18 +9,10 @@ var Class = require("../lib/Attribute/Attribute.js");
 
 var Effect = require("../lib/Effect/Effect.js");
 
-var Characteristics = require("../lib/Characteristics/Characteristics.js");
-
-var CharacteristicsListener = require("../lib/Characteristics/CharacteristicsListener.js");
-
-var Characteristic = require("../lib/Characteristic/Characteristic.js");
-
-var CharactericListener = require("../lib/Characteristic/CharacteristicListener.js");
-
 var Util = require("../lib/Util/Util.js");
 
 exports.testCharacter = function(test){
-    var c = new Character("test");
-    test.ok(c.Name === "test", "Name should be defined");
+    var c = new Character("Josselin");
+    test.ok(c.Name === "Josselin", "Name should be defined");
     test.done();
 };
