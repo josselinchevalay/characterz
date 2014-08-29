67106df672ffb2dea0b77acf2ef17f05b8d61ef8
joss <josselin54.chevalay@gmail.com>
2014-08-29 08:42:36 +0200

    check handlers

--- test/test.js
+++ test/test.js
@@ -17,11 +17,31 @@ exports.testCharacter = function(test){
     test.done();
 };
 
-exports.testCharacterAttributeChange = function(test){
+exports.testCharacterAttributeChange = function(test) {
     var c = new Character("Josselin");
     c.on("change:Name", function(){console.log(arguments);});
     c.Name = "josselin";
-    c.Name = "Josselin";
-    test.ok(c.Name==="Josselin", "Name should be defined");
+    test.ok(c.Name==="josselin", "Name should be defined");
+    test.done();
+};
+
+exports.testCharacterAttributeChangeMultipleHandlers = function(test) {
+    var c = new Character("Josselin");
+    c.on("change:Name", function(){console.log(arguments);});
+    c.on("change:Name", function(){console.log("Multiple !!!")});
+    c.Name = "josselin";
+    test.ok(c.Name==="josselin", "Name should be defined");
+    test.done();
+};
+
+
+exports.testCharacterAttributeChangeHandlersSeparate = function(test) {
+    var c = new Character("Josselin");
+    c.on("change:Name", function(){console.log(arguments);});
+    c.on("change:Name", function(){console.log("Multiple !!!")});
+    c.on("change", function(){console.log("change : %s", arguments)});
+    c.Name = "josselin";
+    c.defineProperty("Endu", true, true, 25);
+    test.ok(c.Name==="josselin", "Name should be defined");
     test.done();
 };
